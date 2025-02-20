5eba6ed9a8f1854bc4fd5bdc75e8b6b98991458a
Camila Alm <camila.salmeidatech@hotmail.com>
2025-02-20 04:19:03 -0500

    add Resort model

--- myresorts/myresorts_app/models.py
+++ myresorts/myresorts_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Resort(models.Model):
+    LIFT_TICKET_CHOICES = [
+        ('Ikon Pass', 'Ikon Pass'),
+        ('Epic Pass', 'Epic Pass'),
+        ('Day Lift Ticket', 'Day Lift Ticket'),
+    ]
+    
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)  
+    address = models.TextField(max_length=250)
+    lift_ticket = models.CharField(max_length=20, choices=LIFT_TICKET_CHOICES, default='Day Lift Ticket')
+
+    def __str__(self):
+        return f"{self.name} ({self.country})"
