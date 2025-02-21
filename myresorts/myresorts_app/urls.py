bded3420b8137001764dbd86804b204c990668e4
Camila Alm <camila.salmeidatech@hotmail.com>
2025-02-20 19:53:35 -0500

    add the delete and update trip functionality

--- myresorts/myresorts_app/urls.py
+++ myresorts/myresorts_app/urls.py
@@ -5,11 +5,12 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('resorts/', views.resort_index, name='resort-index'),
-
     path('resorts/<int:resort_id>/', views.resort_detail, name='resort-detail'),
     path('resorts/create/', views.ResortCreate.as_view(), name='resort-create'),
     path('resorts/<int:pk>/update/', views.ResortUpdate.as_view(), name='resort-update'),
     path('resorts/<int:pk>/delete/', views.ResortDelete.as_view(), name='resort-delete'),
     path('resorts/<int:resort_id>/add-trip/', views.add_trip, name='add-trip'),
+    path('trips/<int:pk>/update/', views.TripUpdate.as_view(), name='trip-update'),
+    path('trips/<int:pk>/delete/', views.TripDelete.as_view(), name='trip-delete'),
 ]
 
--- myresorts/myresorts_app/views.py
+++ myresorts/myresorts_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Resort
+from .models import Resort, Trip
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .forms import TripForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def home(request):
     return render(request, 'home.html')
@@ -20,7 +21,6 @@ def resort_detail(request, resort_id):
     trip_form = TripForm()
     return render(request, 'resorts/detail.html', {'resort': resort, 'trips': trips, 'trip_form': trip_form})  
 
-
 def add_trip(request, resort_id):
     form = TripForm(request.POST)
     if form.is_valid():
@@ -28,10 +28,6 @@ def add_trip(request, resort_id):
         new_trip.resort_id = resort_id
         new_trip.save()
     return redirect('resort-detail', resort_id=resort_id)
-
-
-
-
 
 
 class ResortCreate(CreateView):
@@ -42,9 +38,29 @@ class ResortCreate(CreateView):
 class ResortUpdate(UpdateView):
     model = Resort
     fields = ['country', 'state', 'address', 'lift_ticket']
-    # template_name = 'resorts/resort_form.html'
+
 
 class ResortDelete(DeleteView):
     model = Resort
     success_url = '/resorts/'
-    # template_name = 'resorts/resort_confirm_delete.html'
+
+
+class TripUpdate(UpdateView):
+    model = Trip
+    fields = ['date', 'buddy']
+    def get_success_url(self):
+        trip = self.object 
+        return f'/resorts/{trip.resort.id}/'
+
+
+class TripDelete(DeleteView):  
+    model = Trip
+    template_name = "trips/trip_confirm_delete.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['resort'] = self.object.resort  
+        return context
+
+    def get_success_url(self):
+        return f'/resorts/{self.object.resort.id}/'
